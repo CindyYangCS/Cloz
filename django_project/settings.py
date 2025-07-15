c8817d0d8e8d7a0e0ac12d39bda5364a8d95461c
CindyYangCS <cindyy20@gmail.com>
2025-07-15 10:40:59 -0500

    Added comments system to posts

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_filters',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'closet.apps.ClosetConfig',
